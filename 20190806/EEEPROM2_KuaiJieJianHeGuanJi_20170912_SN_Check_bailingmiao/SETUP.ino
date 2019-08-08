68cff7cc0a0aa56d8e6ab8d936eaf0465d03b6db
newimagery <94043728@qq.com>
2019-08-09 00:58:54 +0800

    整理

--- 20190806/EEEPROM2_KuaiJieJianHeGuanJi_20170912_SN_Check_bailingmiao/SETUP.ino
+++
@@ -1,9 +0,0 @@
-void setup()
-{
-  pinMode(computer, OUTPUT);
-  pinMode(projector, OUTPUT);
-  digitalWrite(computer, HIGH);
-  digitalWrite(projector, HIGH);
-  Serial1.begin(9600);
-  Keyboard.begin();
-}
